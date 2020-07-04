7f664cd72b143768c1ff392ec5612d78f869fa3a
Andrii Ochkurov <andrey4016@gmail.com>
2020-07-04 19:18:26 +0300

    Add simple CRUD

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -1,11 +1,10 @@
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using HPlusSport.API.Classes;
 using HPlusSport.API.Dtabase;
 using HPlusSport.API.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace HPlusSport.API.Controllers
 {
@@ -63,11 +62,64 @@ namespace HPlusSport.API.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
-            var product = await _context.Products.FindAsync(id);
+            var product = await _context.Products.FindAsync(id);    
             if(product == null)
             {
                 return NotFound();
             }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> Post([FromBody]Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                "Get",
+                new { id = product.Id },
+                product
+            );
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            } 
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (_context.Products.Find(id) == null)
+                {
+                    return NotFound();
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
 
             return Ok(product);
         }
--- HPlusSport.API/Models/Product.cs
+++ HPlusSport.API/Models/Product.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace HPlusSport.API.Models
@@ -8,8 +9,10 @@ namespace HPlusSport.API.Models
 
         public string Sku { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [MaxLength(255)]
         public string Description { get; set; }
 
         public decimal Price { get; set; }
